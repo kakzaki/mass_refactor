fdda731c1dca301a3a24abe8c771fa989a09dace
ZAKI MUBAROK <kakzaki@gmail.com>
2023-08-23 17:30:52 +0700

    fix global

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+- Fix global packages
+
 ## 1.0.0
 
 - Initial version.
--- lib/mass_refactor.dart
+++ lib/mass_refactor.dart
@@ -1,10 +1,22 @@
-// Utility function to capitalize the first letter of a string
+/// The official command line interface for mass_refactor
+///
+/// ```sh
+/// # 🎯 Activate mass_refactor
+/// dart pub global activate mass_refactor
+///
+/// # 👀 See usage
+/// mass_refactor --help
+/// ```
+library mass_refactor;
+
+
+/// Utility function to capitalize the first letter of a string
 String capitalize(String input) {
   if (input.isEmpty) return input;
   return input[0].toUpperCase() + input.substring(1);
 }
 
-// Utility function to replace keywords in file content
+/// Utility function to replace keywords in file content
 String replaceKeywordsInContent(
     String content, Map<String, String> keywordReplacements) {
   for (final entry in keywordReplacements.entries) {
